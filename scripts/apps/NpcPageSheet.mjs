237e3f9a6ff808b0b5eb11291e79f3538331e5eb
Cody Swendrowski <cody@ironmoosedevelopment.com>
2024-10-29 18:41:47 -0500

    Add SFX support

--- scripts/apps/NpcPageSheet.mjs
+++ scripts/apps/NpcPageSheet.mjs
@@ -1,9 +1,12 @@
 export default class NpcPageSheet extends JournalPageSheet {
     /** @inheritdoc */
     static get defaultOptions() {
-        const options = super.defaultOptions;
-        options.classes.push("form");
-        return options;
+        return foundry.utils.mergeObject(super.defaultOptions, {
+            classes: ["form", "intelligent-npcs", "actor-configuration"],
+            dragDrop: [
+                {dropSelector: ".playlist"}
+            ],
+        });
     }
 
     /* -------------------------------------------- */
@@ -22,7 +25,7 @@ export default class NpcPageSheet extends JournalPageSheet {
 
         /** @type {FishingSpotData} */
         let config = context.document.flags["intelligent-npcs"] ?? {};
-        if ( foundry.utils.isEmpty(config) ) {
+        if (foundry.utils.isEmpty(config)) {
             config["enabled"] = true;
             config["summary"] = "You are a generic NPC named Bob.";
             config["background"] = "You live in a quiet town where nothing much goes on.";
@@ -32,13 +35,23 @@ export default class NpcPageSheet extends JournalPageSheet {
             config["appearance"] = "Farmer attire";
             config["exampleSentence"] = "<i>Tips hat</i> \"Evening.\"";
             config["memory"] = "Today my wife and I went to the market. We bought some bread and milk.";
+            config["playlist"] = null;
+        }
+
+        let hasPlaylistLink = false;
+        let playlistContentLink = "";
+        if (config["playlist"]) {
+            hasPlaylistLink = true;
+            playlistContentLink = await TextEditor.enrichHTML(`@UUID[${config["playlist"]}]`);
         }
 
         return foundry.utils.mergeObject(context, {
             journal: this.object,
             config: config,
             user: game.user,
-            messageHistory: JSON.stringify(context.document.getFlag("intelligent-npcs", "messageHistory"), null, 2)
+            messageHistory: JSON.stringify(context.document.getFlag("intelligent-npcs", "messageHistory"), null, 2),
+            hasPlaylistLink: hasPlaylistLink,
+            playlistContentLink: playlistContentLink,
         });
     }
 
@@ -55,11 +68,31 @@ export default class NpcPageSheet extends JournalPageSheet {
 
     /* -------------------------------------------- */
 
+    activateListeners(html) {
+        super.activateListeners(html);
+
+        html.find(".playlist-remove").click(async (event) => {
+            event.preventDefault();
+            this.object.update({flags: {"intelligent-npcs": {playlist: null}}});
+        });
+
+        html.find(".content-link").click(async (event) => {
+            event.preventDefault();
+            const uuid = this.object.getFlag("intelligent-npcs", "playlist");
+            if (uuid) {
+                const doc = await fromUuid(uuid);
+                if (doc) doc.sheet.render(true);
+            }
+        });
+    }
+
+    /* -------------------------------------------- */
+
     /** @override */
     _getHeaderButtons() {
         const buttons = super._getHeaderButtons();
 
-        if ( !this.isEditable ) return buttons;
+        if (!this.isEditable) return buttons;
 
         buttons.unshift({
                 label: "Import",
@@ -100,7 +133,7 @@ export default class NpcPageSheet extends JournalPageSheet {
     async _onImportSubmit(html) {
         // Get the file input
         const input = html.find("input[type='file']")[0];
-        if ( !input || !input.files || !input.files.length ) return;
+        if (!input || !input.files || !input.files.length) return;
 
         // Read the file
         const file = input.files[0];
@@ -108,7 +141,7 @@ export default class NpcPageSheet extends JournalPageSheet {
 
         // Read as JSON
         const config = JSON.parse(content);
-        if ( !config ) return;
+        if (!config) return;
 
         // Update the actor
         this.object.flags["intelligent-npcs"] = config;
@@ -124,5 +157,26 @@ export default class NpcPageSheet extends JournalPageSheet {
         const content = JSON.stringify(config, null, 2);
         const filename = `${this.object.name}-intelligent-npc-journal-page-config.json`;
         saveDataToFile(content, "application/json", filename);
+    }
+
+    /* -------------------------------------------- */
+
+    async _onDrop(event) {
+        super._onDrop(event);
+        const data = JSON.parse(event.dataTransfer.getData("text/plain"));
+
+        if (event.currentTarget.classList.contains("playlist")) {
+            const doc = await fromUuid(data.uuid);
+            if (!doc) return;
+            if (doc.documentName !== "Playlist") {
+                ui.notifications.error(`Expected a Playlist Document, but got a ${doc.documentName} one instead. `);
+                return;
+            }
+
+            const flag = this.object.flags["intelligent-npcs"] ?? {};
+            flag["playlist"] = data.uuid;
+            this.object.update({flags: {"intelligent-npcs": flag}});
+            return;
+        }
     }
 }
